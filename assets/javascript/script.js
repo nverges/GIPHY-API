bbdca3dbbe7c3bcd1e45dc8d428968b8f53c8fc0
nverges <nverges@email.arizona.edu>
2017-05-19 12:37:13 -0700

    start

---
+++ assets/javascript/script.js
@@ -0,0 +1,20 @@
+
+// declare variables
+  // array of gifs input
+
+
+// main function
+
+  // process that grabs input value
+
+    // creates a button for each animal input
+
+    // button value is used as api value
+
+  // searches database 
+
+  // returns results to html
+
+
+// function that creates a button for each 
+
