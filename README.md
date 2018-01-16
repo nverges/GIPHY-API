f1445e8fdd3ecc272d655065db780c16d9c5b9e0
nverges <nverges@email.arizona.edu>
2018-01-16 16:45:36 -0700

    added node server

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # GIPHY-API
+
+Deployed at: 
+
+A silly app using vanilla JavaScript and jQuery to allow the user to search for a GIF.
+
+The input request will create a button with that name.
+
+When the button is clicked, the script will hit the GIPHY API and return 10 results.
+
+
+
+
+Enjoy!
---
+++ server.js
@@ -0,0 +1,17 @@
+let express = require('express');
+let path = require('path');
+
+const app = express();
+
+const PORT = process.env.port || 3000;
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.resolve(__dirname + '/index.html'));
+});
+
+
+app.listen (PORT, () => {
+    console.log(`App listening on PORT ${PORT}`)
+});
